109ef993b6be232a1a6c64adf1944bda904d4e0d
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-08 05:12:11 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,19 @@
+""" Turn every item of a list into its square
+
+Given a list of numbers. write a program to turn every item of a list into its square.
+
+Given:
+
+numbers = [1, 2, 3, 4, 5, 6, 7] """
+
+
+#!/usr/bin python3
+numbers = [1, 2, 3, 4, 5, 6, 7]
+r=numbers[0]*1
+q=numbers[1]*2
+w=numbers[2]*3
+t=numbers[3]*4
+y=numbers[4]*5
+o=numbers[5]*6
+p=numbers[6]*7
+print([r,q,w,t,y,o,p])
