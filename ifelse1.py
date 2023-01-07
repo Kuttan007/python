63ce4b7dd3bef384af054feb4e1cfcdcbdcafc9c
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-08 04:13:29 +0530

    Update and rename ifelse1.py to sum.py

--- ifelse1.py
+++
@@ -1,6 +0,0 @@
-	#!/usr/bin python3
-s=0
-n = int (input("Enter a value: "))
-for i in range(1,n+1):
-        s=s+i
-print(s)
---
+++ sum.py
@@ -0,0 +1,8 @@
+""" Calculate the sum of all numbers from 1 to a given number """
+
+#!/usr/bin python3
+s=0
+n = int (input("Enter a value: "))
+for i in range(1,n+1):
+        s=s+i
+print(s)
