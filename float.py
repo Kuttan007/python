ec02b21d6682f29da607c4c3b2e309116afdc909
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-08 06:40:07 +0530

    Create float.py

---
+++ float.py
@@ -0,0 +1,9 @@
+""" Exercise 5: Accept a list of 5 float numbers as an input from the user """
+
+#!/usr/bin python3
+a=float(input("Enter float number"))
+b=float(input("Enter float number"))
+c=float(input("Enter float number"))
+d=float(input("Enter float number"))
+e=float(input("Enter float number"))
+print(a,b,c,d,e)
