af09e4760dc766b87302452a9a90d05749fba01a
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-09 01:10:46 +0530

    Create check if 2 strings are balanced.py

---
+++ check if 2 strings are balanced.py
@@ -0,0 +1,20 @@
+""" String characters balance Test
+
+Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter.
+
+Given:
+
+Case 1:
+
+s1 = "Yn"
+s2 = "PYnative"
+"""
+
+#!/usr/bin python3
+s1 = "Yn"
+s2= "PYnative"
+if s1 in s2:
+  print("True")
+else:
+  print("False")
+
