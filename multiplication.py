7c8d7195f7bb0de6da9e62dbe25581a6f49d3dee
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-08 06:02:08 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+""" Write a program to accept two numbers from the user and calculate multiplication """
+
+#!/usr/bin python3
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter second number "))
+
+res = num1 * num2
+print("Multiplication is", res)
