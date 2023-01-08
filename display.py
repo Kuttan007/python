ba55f1476efc4b6b539984bf9e7b3b62be1be64d
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-08 06:07:10 +0530

    Create display.py

---
+++ display.py
@@ -0,0 +1,8 @@
+""" Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+
+Expected Output:
+
+For example: print('Name', 'Is', 'James') will display Name**Is**James """
+
+#!/usr/bin python3
+print("Name**Is**James")
