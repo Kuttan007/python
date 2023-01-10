b0ede88da5164007ed51bb371c6818d5a572dee8
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-10 08:12:19 +0530

    appendstring

---
+++ appendstring.py
@@ -0,0 +1,17 @@
+""" Append new string in the middle of a given string
+
+Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
+
+Given:
+
+s1 = "Ault"
+s2 = "Kelly"
+
+"""
+
+#!/usr/bin python3
+s1= "Ault"
+s2= "Kelly"
+s3= s1[0:2]
+s4= s1[2:4]
+print(s3+s2+s4)
