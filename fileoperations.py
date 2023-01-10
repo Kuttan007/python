7eb064a6b223b237e558e1ab502b7d552092063e
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-10 08:18:44 +0530

    Update fileoperations.py

--- fileoperations.py
+++ fileoperations.py
@@ -16,19 +16,19 @@ line7
 """
 
 with open("/tmp/test.txt", "r") as fp:
-    # read all lines from a file
+
         lines = fp.readlines()
 
-# open new file in write mode
+
 with open("/tmp/test1.txt", "w") as fp:
         count = 0
-    # iterate each lines from a test.txt
+    
         for line in lines:
                 count +=1
 
-        # skip 5th lines
+       
         if count == 4:
                 continue
         else:
-            # write current line
-                fp.write(line)
+          
+            fp.write(line)
