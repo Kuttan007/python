31515be61cc8c4ae8823380b8e5468d41705b809
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-10 08:25:55 +0530

    Create split.py

---
+++ split.py
@@ -0,0 +1,14 @@
+""" Expected Output
+
+Enter three string Emma Jessa Kelly
+Name1: Emma
+Name2: Jessa
+Name3: Kelly
+"""
+
+
+#!/usr/bin python3
+str1, str2, str3 = input("Enter three string").split()
+print('Name1:', str1)
+print('Name2:', str2)
+print('Name3:', str3)
