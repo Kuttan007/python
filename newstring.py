c54a0ce17baeb14402072c447e5680a646385542
Nithin Chandran <104108465+Kuttan007@users.noreply.github.com>
2023-01-10 08:14:37 +0530

    Create newstring.py

---
+++ newstring.py
@@ -0,0 +1,18 @@
+""" Write a program to create a new string made of an input string’s first, middle, and last character.
+
+Given:
+
+str1 = "James"
+
+Expected Output:
+
+Jms
+"""
+
+
+#!/usr/bin python3
+str1="James"
+str2=str1[0]
+str3=str1[2]
+str4=str1[4]
+print(f"{str2}{str3}{str4}")
